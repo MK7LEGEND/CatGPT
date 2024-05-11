a3c90e0a1f846689ca9bfff6f66658026147b5fc
Muhammad Murash Khan <140887384+MK7LEGEND@users.noreply.github.com>
2024-05-12 02:38:06 +0500

    Add files via upload

---
+++ responses.py
@@ -0,0 +1,18 @@
+import random
+
+
+def handle_response(message) -> str:
+    p_message = message.lower()
+    if p_message == 'hello':
+        return 'Meow!'
+
+    if p_message == 'roll':
+        return str(random.randint(1, 6))
+
+    if p_message == 'help':
+        return "meow meow meow meow"
+
+    if p_message == '!help'
+        return "`sry no help for you.`"
+
+    #  return 'Yeah, I don\'t know. Try typing "!help".'
